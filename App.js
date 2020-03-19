11b46ab40a3b3523532ee103268d6ad01d58b3cc
roberto araya gil <naggas@Naggas.local>
2020-03-19 21:32:07 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>5 Tenedores!</Text>
+      <Button
+        title=" click me"
+        icon={<Icon name="arrow-right" size={15} color="white" />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
